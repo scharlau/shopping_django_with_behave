af965ac9ac9fc66778cefabd966b258a28e19279
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2022-11-04 14:02:14 +0000

    first version working

--- features/environment.py
+++ features/environment.py
@@ -20,15 +20,16 @@ CHROME_DRIVER = os.path.join(current_dir, 'driver/chromedriver')
 chrome_options = Options()
 # comment out the line below if you want to see the browser launch for tests
 # possibly add time.sleep() if required
-chrome_options.add_argument("--headless")
+#chrome_options.add_argument("--headless")
 chrome_options.add_argument('--no-proxy-server')
 chrome_options.add_argument("--proxy-server='direct://'")
 chrome_options.add_argument("--proxy-bypass-list=*")
 
 def before_all(context):
     use_fixture(django_test_runner, context)
-    driver = webdriver.Chrome(options=chrome_options, executable_path=CHROME_DRIVER)
-    driver.set_page_load_timeout(time_to_wait=200)
+    browser = webdriver.Chrome(options=chrome_options, executable_path=CHROME_DRIVER)
+    browser.set_page_load_timeout(time_to_wait=200)
+    context.browser = browser
 
 def before_scenario(context, scenario):
     context.test = TestCase()
@@ -40,8 +41,7 @@ def after_scenario(context, scenario):
     del context.test
 
 def after_all(context):
-    driver = webdriver.Chrome(options=chrome_options, executable_path=CHROME_DRIVER)
-    driver.quit()
+    context.browser.quit()
 
 @fixture
 def django_test_runner(context):
--- features/steps/product.py
+++ features/steps/product.py
@@ -1,29 +1,49 @@
+import urllib
+from urllib.parse import urljoin
+from behave import *
 
-@given(u'we want to add a product')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given we want to add a product')
+@given( "we want to add a product")
+def user_on_product_newpage(context):
+    base_url = urllib.request.url2pathname(context.test_case.live_server_url)
+    print(base_url)
+    open_url = urljoin(base_url,'/product_new/')
+    context.browser.get(open_url)
 
+@when( "we fill in the form")
+def user_fills_in_the_form(context):
+    # use print(context.browser.page_source) to aid debugging
+    print(context.browser.page_source)
+    name_textfield = context.browser.find_element('name', 'name')
+    name_textfield.send_keys('thing one')
+    price_textfield = context.browser.find_element('name','price')
+    price_textfield.send_keys(3)
+    context.browser.find_element('name','submit').click()
 
-@when(u'we fill in the form')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When we fill in the form')
-
-
-@then(u'it succeeds')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then it succeeds')
-
+@then( "it succeeds")
+def product_added(context):
+    assert 'thing one' in context.browser.page_source
 
 @given(u'we have specific products to add')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given we have specific products to add')
-
-
+def specific_products(context):
+    base_url = urllib.request.url2pathname(context.test_case.live_server_url)
+    open_url = urljoin(base_url,'/product_new/')
+    for row in context.table:
+        context.browser.get(open_url)
+        name_textfield = context.browser.find_element('name', 'name')
+        name_textfield.send_keys(row['name'])
+        price_textfield = context.browser.find_element('name','price')
+        price_textfield.send_keys(row['price'])
+        context.browser.find_element('name','submit').click()
+        assert row['name'] in context.browser.page_source
+       
 @when(u'we visit the listing page')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When we visit the listing page')
-
+    base_url = urllib.request.url2pathname(context.test_case.live_server_url)
+    open_url = urljoin(base_url,'/product_list')
+    context.browser.get(open_url)
+    print(context.browser.page_source)
+    assert 'Product List' in context.browser.page_source
 
 @then(u'we will find \'another thing\'')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then we will find \'another thing\'')
+    assert 'another thing' in context.browser.page_source
